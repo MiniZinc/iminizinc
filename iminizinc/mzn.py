35950d445b983a02c26132ea7603381c896497c7
Guido Tack <guido.tack@monash.edu>
2018-03-11 14:01:32 +1100

    Support for different options

--- iminizinc/mzn.py
+++ iminizinc/mzn.py
@@ -3,33 +3,107 @@ from IPython.core.magic import (Magics, magics_class, line_magic,
                                 cell_magic, line_cell_magic)
 from IPython.utils.tempdir import TemporaryDirectory
 from IPython.core.display import HTML
+from IPython.core import magic_arguments
 from notebook.services.config.manager import ConfigManager
 import subprocess, os, sys
 import json
+import re
 
 @magics_class
 class MznMagics(Magics):
 
-    @cell_magic
-    def minizinc(self, line, cell):
-        "minizinc cell magic"
-        if line=="gecode":
-            fzncommand = "fzn-gecode"
+    @magic_arguments.magic_arguments()
+    @magic_arguments.argument(
+        '-v',
+        '--verbose',
+        action='store_true',
+        help='Verbose output'
+    )
+    @magic_arguments.argument(
+        '-s',
+        '--statistics',
+        action='store_true',
+        help='Output statistics'
+    )
+    @magic_arguments.argument(
+        '-o',
+        '--solution-object',
+        default=None,
+        help='Return solution(s) in this variable'
+    )
+    @magic_arguments.argument(
+        '-a',
+        '--all-solutions',
+        action='store_true',
+        help='Return all solutions for satisfaction problems, intermediate solutions for optimisation problems. Implies -o.'
+    )
+    @magic_arguments.argument(
+        '-t',
+        '--timeout',
+        type=int,
+        help='Timeout (in seconds)'
+    )
+    @magic_arguments.argument(
+        '--solver',
+        choices=["gecode","cbc"],
+        default="gecode",
+        help='Solver to run'
+    )
+    @magic_arguments.argument(
+        '--data',
+        nargs='*',
+        default=[],
+        help='Data files'
+    )
+    @magic_arguments.argument(
+        'model',
+        nargs='*',
+        default=[],
+        help='Model to solve'
+    )
+
+    @line_cell_magic
+    def minizinc(self, line, cell=None):
+        "minizinc magic"
+        args = magic_arguments.parse_argstring(self.minizinc, line)
+        solver = []
+        if args.solver=="gecode":
+            solver = ["fzn-gecode"]
+            if args.timeout:
+                solver.append("-time")
+                solver.append(args.timeout*1000)
             mznlib = "-Ggecode"
-        elif line=="cbc":
-            fzncommand = "mzn-cbc"
+        elif args.solver=="cbc":
+            solver = ["mzn-cbc"]
+            if args.timeout:
+                solver.append("--timeout")
+                solver.append(args.timeout)
             mznlib = "-Glinear"
         else:
-            fzncommand = "fzn-gecode"
-            mznlib = "-Ggecode"            
-
+            print("No solver given")
+            return
+        
+        mzn2fzn = ["mzn2fzn",mznlib]
+        if args.verbose:
+            mzn2fzn.append("-v")
+        if args.statistics:
+            mzn2fzn.append("-s")
+        
+        if args.statistics:
+            solver.append("-s")
+        if args.all_solutions:
+            solver.append("-a")
+        
         my_env = os.environ.copy()
 
+        cwd = os.getcwd()
+        
         with TemporaryDirectory() as tmpdir:
             with open(tmpdir+"/model.mzn", "w") as modelf:
-                modelf.write(cell)
+                if cell is not None:
+                    modelf.write(cell)
                 modelf.close()
-                pipes = subprocess.Popen(["mzn2fzn",mznlib,"--model-interface-only",tmpdir+"/model.mzn"],
+                pipes = subprocess.Popen(mzn2fzn+["--model-interface-only",tmpdir+"/model.mzn"]+args.model+args.data,
                                          stdout=subprocess.PIPE,stderr=subprocess.PIPE,env=my_env)
                 (output,erroutput) = pipes.communicate()
                 if pipes.returncode != 0:
@@ -50,7 +124,8 @@ class MznMagics(Magics):
                     with open(tmpdir+"/data.json", "w") as dataf:
                         json.dump(bindings, dataf)
                         dataf.close()
-                        pipes = subprocess.Popen(["mzn2fzn",mznlib,"--output-mode","json",tmpdir+"/model.mzn",tmpdir+"/data.json"],
+                        pipes = subprocess.Popen(mzn2fzn+["--output-mode","json",tmpdir+"/model.mzn"]+args.model+
+                                                 [tmpdir+"/data.json"]+args.data,
                                                  stdout=subprocess.PIPE,stderr=subprocess.PIPE,env=my_env)
                         (output,erroutput) = pipes.communicate()
                         if pipes.returncode != 0:
@@ -58,11 +133,11 @@ class MznMagics(Magics):
                             return
                         if len(erroutput) != 0:
                             print(erroutput)
-                        pipes = subprocess.Popen([fzncommand,tmpdir+"/model.fzn"],
+                        pipes = subprocess.Popen(solver+[tmpdir+"/model.fzn"],
                                                  stdout=subprocess.PIPE,stderr=subprocess.PIPE,env=my_env)
                         (fznoutput,erroutput) = pipes.communicate()
                         if pipes.returncode != 0:
-                            print("Error in "+fzncommand+":\n"+erroutput)
+                            print("Error in "+solver[0]+":\n"+erroutput)
                         if len(erroutput) != 0:
                             print(erroutput)
                         with open(tmpdir+"/model.ozn","r") as oznfile:
@@ -85,15 +160,40 @@ class MznMagics(Magics):
                             return
                         if len(erroutput) != 0:
                             print(erroutput)
-                        solutions = json.loads("["+solns2output+"]")
-                        if len(solutions)==0:
-                            print("No solutions found")
-                            return
-                        solution = solutions[-1]
+                        # Remove comments from output
+                        cleanoutput = []
+                        commentsoutput = []
+                        for l in solns2output.splitlines():
+                            comment = re.search(r"^\s*%+\s*(.*)",l)
+                            if comment:
+                                commentsoutput.append(comment.group(1))
+                            else:
+                                cleanoutput.append(l)
+                        solutions = json.loads("["+"".join(cleanoutput)+"]")
                         bindings = []
-                        for var in solution:
-                            self.shell.user_ns[var] = solution[var]
-                            bindings.append(var+"="+str(solution[var]))
+                        if args.solution_object:
+                            if args.all_solutions:
+                                self.shell.user_ns[args.solution_object] = solutions
+                                bindings.append(args.solution_object+"="+str(solutions))
+                            else:
+                                if len(solutions)==0:
+                                    self.shell.user_ns[args.solution_object] = None
+                                    bindings.append(args.solution_object+"=None")
+                                else:
+                                    self.shell.user_ns[args.solution_object] = solutions[-1]
+                                    bindings.append(args.solution_object+"="+str(solutions[-1]))
+                        else:
+                            if len(solutions)==0:
+                                print("No solutions found")
+                                return
+                            solution = solutions[-1]
+                            for var in solution:
+                                self.shell.user_ns[var] = solution[var]
+                                bindings.append(var+"="+str(solution[var]))
+                        if len(commentsoutput) > 0:
+                            print("Solver output:")
+                            print("\n".join(commentsoutput))
+                            print()
                         print("\n".join(bindings))
                         return
                         
