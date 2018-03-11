3f239be09bbba8ba2d112e378f5374cb5feb2b51
Guido Tack <guido.tack@monash.edu>
2018-03-11 11:50:48 +1100

    Add classifiers and keywords

--- setup.py
+++ setup.py
@@ -14,5 +14,32 @@ setup(
     author_email='guido.tack@monash.edu',
     packages=['iminizinc'],
     install_requires=['ipython'],
-    include_package_data=True
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Operating System :: OS Independent',
+        
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords = 'minizinc ipython',
+    project_urls={
+        'Source': 'https://github.com/MiniZinc/iminizinc/',
+        'Tracker': 'https://github.com/MiniZinc/iminizinc/issues',
+    },
 )
