5d9771499752e0ad3cf0a7e9a2e7e910537808b9
Jip J. Dekker <jip@dekker.li>
2018-04-03 15:50:57 +1000

    Update statistics flag description to match new functionality

--- iminizinc/mzn.py
+++ iminizinc/mzn.py
@@ -24,7 +24,7 @@ class MznMagics(Magics):
         '-s',
         '--statistics',
         action='store_true',
-        help='Output statistics'
+        help='Return compiler and solver statistics. Statistics will be bound to "statistics" when using -m bound'
     )
     @magic_arguments.argument(
         '-m',
